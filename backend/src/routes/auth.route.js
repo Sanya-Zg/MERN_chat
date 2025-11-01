710ac05f3bac2f1b15f76927d04aaf500a31d791
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-11-01 17:07:24 +0200

    first commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.send("Login endpoint!");
+})
+router.get("/signup", (req, res) => {
+  res.send("Signup endpoint!");
+})
+
+
+export default router;
