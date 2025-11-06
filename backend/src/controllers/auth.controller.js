48e444e5ca00abcd47ec0a10f0d7a5ac2ad82c21
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-11-06 20:08:27 +0200

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,6 +68,12 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    if (!email?.trim() || !password?.trim()) {
+      return res
+        .status(400)
+        .json({ message: 'Email and password are required' });
+    }
+
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
 
@@ -76,6 +82,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
 
     generateToken(user._id, res);
+
     res.status(200).json({
       _id: user._id,
       fullName: user.fullName,
@@ -84,11 +91,11 @@ export const login = async (req, res) => {
     });
   } catch (error) {
     console.error('Error in login controller:', error);
-    res.status(500).json({ message: 'Internal server error'})
+    res.status(500).json({ message: 'Internal server error'});
   }
 };
 
-export const logout = async (_, res) => {
-  res.cookie('jwt', '', { maxAge: 0});
-  res.status(200).json({ message: 'Logged out successfully'})
+export const logout = (_, res) => {
+  res.cookie('jwt', '', { maxAge: 0 });
+  res.status(200).json({ message: 'Logged out successfully' });
 };
